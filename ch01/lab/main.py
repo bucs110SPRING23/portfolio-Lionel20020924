6dffeef1d765c9937bec3ec2e63709bb78a02616
Q Lionel <qlionel@QdeMacBook-Pro.local>
2023-02-11 17:38:32 -0500

    my commit message

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,15 @@
+# Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print (" Cost per week : ", cost_per_week)
+classes_per_week = 3 
+cost_per_class = ( cost_per_week / classes_per_week)
+print("Cost per class :", cost_per_class)
 
+#Part B
+import random
+list = ["apple", 10, 55.2, "pig", 0]
+a = random.choice(list)
+print (a)
